e410473e17680cedf73b6845096fcd3d14a12e0b
Mildred Erubore <derubore@gmail.com>
2023-06-07 20:14:55 +0100

    First commit and instaled tailwind

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/main.css'
+
+// interface props {
+//   image: String,
+// }
+
+function Header () {
+  return (
+    <div className="text-blue-700 underline">
+      Header
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+
+function Login () {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Login;
