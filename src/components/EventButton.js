7aebcedccc2065edc069d9fba4feab71ce7ee6b0
Mildred Erubore <derubore@gmail.com>
2023-06-14 22:59:51 +0100

    finished working on the home page

---
+++ src/components/EventButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../styles/main.css'
+import { Link } from 'react-router-dom'
+
+export const EventButton = ({title }) => {
+  return (
+    <div className='flex justify-center pb-6'>
+      <button className='text-white text-base w-full bg-[#0052F6] block px-6 py-5 rounded-xl'>
+        <Link to='/eventlistings'>
+        {title}
+        </Link>
+      </button>
+    </div>
+  )
+}
+
+EventButton.propTypes = {
+  title: PropTypes.string
+}
