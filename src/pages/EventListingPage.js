953f459121c50a72ffec0fc66b72bf695e9b34fc
Mildred Erubore <derubore@gmail.com>
2023-06-15 01:46:31 +0100

    linked the images in the components

--- src/pages/EventListingPage.js
+++ src/pages/EventListingPage.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-
+import Search from '../components/Search'
 
 function EventListingPage () {
   return (
-    <div>
+    <div className='font-monrope'>
       <Header/>
+      <div className='mx-auto w-[100%] min-[548px]:w-[60%] lg:w-[30%] border'>
+        <p className='font-light text-2xl mt-16 mb-4 pl-4'>
+          Discover events near you
+        </p>
+        <div>
+          <Search/>
+        </div>
+      </div>
       <Footer/>
     </div>
-  );
+  )
 }
 
-export default EventListingPage;
+export default EventListingPage
