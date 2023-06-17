8ace555bb8cfc644922aae0bae88034d32ab66e6
Mildred Erubore <derubore@gmail.com>
2023-06-17 13:08:14 +0100

    made changes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -11,7 +11,7 @@ function LandingPage () {
     <div className='landingpage font-manrope bg-[#030A2E]'>
       <div className='mx-auto w-[90%] min-[548px]:w-[60%] lg:w-[30%]'>
         <div style={{textAlign: 'center', paddingTop: '8em'}}>
-          <Link to='/login'>
+          <Link to='/landingpage'>
           <img src={logo} alt='logo' style={{width: '40%', margin: '0 auto 4em auto'}} />
           </Link>
         </div>
@@ -22,7 +22,7 @@ function LandingPage () {
           <p className='text-lg text-center' style={{ fontSize: '20px', margin: '2em auto 2em auto', color: 'white' }}>
             Your one stop platform to create , manage and promote your events at your convenience.
           </p>
-          <EventButton to='home' title={"Create an Event"} />
+          <EventButton to='login' title={"Create an Event"} />
           <div className='flex justify-center w-full'>
             <button className='text-black text-base w-full bg-white block px-6 py-5 rounded-xl'>
               <Link to='/eventlistings'> Discover events
